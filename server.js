068b68d0d425f3ae6df8e6d732b169eab578bbbb
Vita Kozlovska <Vita_Kozlovska@epam.com>
2020-12-13 22:17:23 +0200

    Changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('./dist/clicker'));
 app.get('/*', function (req, res) {
   res.sendFile('index.html', {root: 'dist/clicker/'});
 });
-app.listen(process.env.PORT || 8080);
+app.listen(PORT);
